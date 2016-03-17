edd4f00a394fc1270e19324ec06f4b02eb7ef011
epatter1 <elishajp20@gmail.com>
2016-03-17 10:09:58 -0400

    Hex values display in textview and text dynamically contrasts with background

--- app/src/main/java/edu/ggc/epatter1/grizzlycolors/MainActivity.java
+++ app/src/main/java/edu/ggc/epatter1/grizzlycolors/MainActivity.java
@@ -27,13 +27,6 @@ public class MainActivity extends AppCompatActivity {
     public SeekBar greenSeekBar;
     private SeekBar alphaSeekBar;
     private TextView swatchTextView;
-    private Color color = new Color();
-    private int redProgress, greenProgress, blueProgress;
-    private String hex;
-
-    public MainActivity() {
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +35,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        init();
+    }
+
+    private void init() {
+        setImageBackgroundImageAlpha();
+        createSeekBars();
+
+        swatchTextView = (TextView) findViewById(R.id.swatchTextView);
+        //TODO need to initialize textView setText with default value
+        swatchTextView.setText("#00000000");
+
+        createSeekBar(redSeekBar);
+        createSeekBar(greenSeekBar);
+        createSeekBar(blueSeekBar);
+        createSeekBar(alphaSeekBar);
+        createAboutButton();
+    }
+
+
+    private void setImageBackgroundImageAlpha() {
+
+        View backgroundView = (View) findViewById(R.id.MainView);
+        backgroundView.getBackground().setAlpha(120);
+
+    }
+
+    private void createSeekBars() {
+
+        //TODO need to set sliders to default position
         redSeekBar = (SeekBar) findViewById(R.id.redSeekBar);
         redSeekBar.setMax(255);
 
@@ -50,124 +72,12 @@ public class MainActivity extends AppCompatActivity {
 
         greenSeekBar = (SeekBar) findViewById(R.id.greenSeekBar);
         greenSeekBar.setMax(255);
+
         alphaSeekBar = (SeekBar) findViewById(R.id.alphaSeekBar);
+        alphaSeekBar.setMax(255);
+    }
 
-
-        swatchTextView = (TextView) findViewById(R.id.swatchTextView);
-
-
-        redSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                redProgress = progress;
-                hex = String.format("#%02x%02x%02x", redProgress, greenProgress, blueProgress);
-                Log.d("RED PROGRESS", ": " + progress);
-                swatchTextView.setBackgroundColor(Color.parseColor(hex));
-//                swatchTextView.setTextColor();
-
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-
-        blueSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                blueProgress = progress;
-                hex = String.format("#%02x%02x%02x", redProgress, greenProgress, blueProgress);
-                Log.d("BLUE PROGRESS", ": " + progress);
-                swatchTextView.setBackgroundColor(Color.parseColor(hex));
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        greenSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                greenProgress = progress;
-                hex = String.format("#%02x%02x%02x", redProgress, greenProgress, blueProgress);
-                Log.d("GREEN PROGRESS", ": " + progress);
-                swatchTextView.setBackgroundColor(Color.parseColor(hex));
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-
-
-        });
-
-        alphaSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                float value = ((float) progress/100.0f );
-//                String s = String.valueOf(value);
-//                int number = Integer.parseInt(s);
-                hex = Integer.toHexString(progress) + hex;
-                swatchTextView.setAlpha(value);
-// setBackgroundColor(Color.parseColor(hex));
-
-//                swatchTextView.setAlpha(Float.valueOf(hex));
-//                Log.i("alpha seekbar value: ", String.valueOf(value));
-            }
-            //format to hex value
-            public void updateHex (int a, int r, int g, int b){
-                swatchTextView = (TextView) findViewById(R.id.swatchTextView);
-                String alphaHex = String.format("%02X", a);
-                String redHex = String.format("%02X", r);
-                String greenHex = String.format("%02X", g);
-                String blueHex = String.format("%02X", b);
-
-                String hexCode = "#" + alphaHex + redHex + greenHex + blueHex;
-                swatchTextView.setText(hexCode);
-                swatchTextView.setBackgroundColor(Color.argb(a,r,g,b));
-                swatchTextView.setTextColor(Color.rgb(255 - r, 255 - g, 255 - b));
-
-
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-
-
-
+    private void createAboutButton() {
 
         Button about = (Button) findViewById(R.id.btnAbout);
         about.setOnClickListener(new View.OnClickListener() {
@@ -177,11 +87,59 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, About.class));
             }
         });
+    }
 
+
+    private void createSeekBar(SeekBar seekbar) {
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                changeTextView();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+
+    private void changeTextView() {
+    //TODO get current value off of the seekBar
+         int redProgress = redSeekBar.getProgress();
+         int greenProgress = greenSeekBar.getProgress();
+         int blueProgress = blueSeekBar.getProgress();
+         int alphaProgress = alphaSeekBar.getProgress();
+
+        // determine the color for the background
+
+        int backgroundColor = Color.argb(alphaProgress, redProgress, greenProgress, blueProgress);
+
+        //inverse the color from the text so we can see it at all times
+        int textColor = Color.argb(255, 255 - redProgress, 255 - greenProgress, 255 - blueProgress);
+
+
+//apply color to the background
+
+        swatchTextView.setBackgroundColor(backgroundColor);
+
+        // set the color on the text and NOT the background
+        swatchTextView.setTextColor(textColor);
+
+        //this is where we set the text in the format FFFFFFFF
+
+        swatchTextView.setText(String.format("#%02x%02x%02x%02x", alphaProgress, redProgress, greenProgress, blueProgress));
 
     }
 }
-
 
 
 
